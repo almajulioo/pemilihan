97e226d37d2117203fa7560afcdb1b671e9ae2a4
almajulioo <alma.122140057@student.itera.ac.id>
2024-09-25 11:13:11 +0700

    feat : add constrain

--- database/migrations/2024_09_24_152838_create_votes_table.php
+++ database/migrations/2024_09_24_152838_create_votes_table.php
@@ -13,9 +13,9 @@ return new class extends Migration
     {
         Schema::create('votes', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('pemilihan_id');
-            $table->foreignId('kandidat_id');
-            $table->foreignId('user_id');
+            $table->foreignId('pemilihan_id')->constrained('pemilihans')->onDelete('cascade');
+            $table->foreignId('kandidat_id')->constrained('kandidats')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
